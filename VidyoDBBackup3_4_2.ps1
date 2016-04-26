ef56bddd931b881d56976b40ed95bb7405a267dd
mckayclarey <mclarey@e-idsolutions.com>
2016-04-26 14:45:28 -0400

    Update VidyoDBBackup3_4_2.ps1
    Added comments.

--- VidyoDBBackup3_4_2.ps1
+++ VidyoDBBackup3_4_2.ps1
@@ -1,16 +1,17 @@
-﻿$portalURL = 'portal.vidyo.com'
+#set these variables for your specific VidyoPortal
+$portalURL = 'portal.vidyo.com'
 $user = 'super'
 $pass = '********'
 
+#prepping the username and password for sending the http requests to the Portal
 $pair = "$($user):$($pass)"
-
 $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
-
 $basicAuthValue = "Basic $encodedCreds"
-
 $Headers = @{
     Authorization = $basicAuthValue
 }
+
+#soap request body
 [xml]$soaprequest = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sup="http://portal.vidyo.com/superapi/">
    <soapenv:Header/>
    <soapenv:Body>
@@ -20,10 +21,15 @@ $Headers = @{
    </soapenv:Body>
 </soapenv:Envelope>'
 
+#soap request to make the db backup file and return the file URL 
 $response = Invoke-WebRequest https://$portalURL/services/VidyoPortalSuperService -Headers $Headers -Method Post -ContentType "text/xml" -Body $soaprequest
 
+#capture just the xml content of the response and store as xml 
 [xml]$xml = $response.Content
 
+#grab the URL of the DB backup file 
 $url = $xml.Envelope.Body.backupDbResponse.databaseBackup.backupURL
+#store the filename portion of the URL for labelling the file
 $filename = $url.Replace("https://$portalURL/super/backups/","")
+#store the file as a variable for the heck of it and output it to the current working directory 
 $dbfile = Invoke-WebRequest $url -Headers $Headers -Method Get -OutFile $filename
